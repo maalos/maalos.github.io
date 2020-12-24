744441842050f3555bc6556073f675bfcc8c2742
maalos <54448857+maalos@users.noreply.github.com>
2020-12-24 13:09:57 +0100

    Update browserchecker.js

--- files/browserchecker.js
+++ files/browserchecker.js
@@ -2,6 +2,6 @@ function init(){
     var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
   
     if(!isChrome) {
-        document.getElementById(browser-warning).innerHTML = '<div id="browser-warning" style="text-align: center; border: 0.45vh solid #FF0000; font-size: 2.5vh; border-radius: 1vh; width: 45vw; position: fixed; margin: 0 auto; left: 0; right: 0;">You are using a non-chromium-based browser! Some site functionality might be gone due to the gayness of other browsers.</div>';
+        document.body.innerHTML = '<div id="browser-warning" style="text-align: center; border: 0.45vh solid #FF0000; font-size: 2.5vh; border-radius: 1vh; width: 45vw; position: fixed; margin: 0 auto; left: 0; right: 0;">You are using a non-chromium-based browser! Some site functionality might be gone due to the gayness of other browsers.</div>';
     }
   }
