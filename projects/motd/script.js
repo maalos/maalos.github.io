3c9aaeb97247cbcd407e2e2f89f4cc89ebe97449
maalos <54448857+maalos@users.noreply.github.com>
2021-01-05 12:36:13 +0100

    Update script.js

--- projects/motd/script.js
+++ projects/motd/script.js
@@ -2,5 +2,5 @@ fetch('https://maalos.ga/projects/motd/motd.txt',)
     .then(res => res.blob())
     .then(blob => blob.text())
     .then(text => {
-    document.getElementById('motd').innerHTML = motd;
+    document.getElementById('motd').innerHTML = text;
 })
