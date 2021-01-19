4c8defa26a24f504d1101a1add21a62d630e649c
maalos <54448857+maalos@users.noreply.github.com>
2021-01-19 04:09:16 -0600

    Create modal.js

---
+++ files/modal.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("dc-modal");
+
+var btn = document.getElementById("dc-link");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
