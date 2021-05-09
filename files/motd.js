05c602f4617a76afc8c609a3c4f8c0e3e11777de
maalos <54448857+maalos@users.noreply.github.com>
2021-05-09 22:54:06 +0200

    Delete motd.js

--- files/motd.js
+++
@@ -1,6 +0,0 @@
-fetch('https://maalos.ga/files/motd.txt',)
-    .then(res => res.blob())
-    .then(blob => blob.text())
-    .then(text => {
-    document.getElementById('motd').innerHTML = text;
-})
