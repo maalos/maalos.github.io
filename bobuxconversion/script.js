6acb815ecfba4280d16701571720931490d9ada2
maalos <54448857+maalos@users.noreply.github.com>
2021-05-11 10:28:40 +0200

    Delete bobuxconversion directory

--- bobuxconversion/script.js
+++
@@ -1,8 +0,0 @@
-function convert() {
-  setTimeout(() => {
-    var bobux = document.getElementById("amount").value;
-    var dollars = bobux * 420.69;
-    document.getElementById("dollarsamount").value = dollars;
-    convert();
-  }, 500);
-}
